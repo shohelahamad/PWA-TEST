f853701eccf3c500de2e0b27e69d9909755dad87
Md Shohel Ahamad <shohel.ahamad@alphajump.de>
2021-08-31 09:00:35 +0200

    PWA updated

--- sw.js
+++ sw.js
@@ -1,58 +1,25 @@
-var APP_PREFIX = 'meinOrtTest' // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_01' // Version of the off-line cache (change this value everytime you want to update cache)
-var CACHE_NAME = APP_PREFIX + VERSION
-var URLS = [ // Add URL you want to cache in this list.
-    '/PWA-TEST/', // If you have separate JS/CSS files,
-    '/PWA-TEST/index.html' // add path to those files here
-]
+var cacheName = 'hello-pwa';
+var filesToCache = [
+    './index.html',
+    './css/style.css',
+    './js/main.js'
+];
 
-// Respond with cached resources
-self.addEventListener('fetch', function(e) {
-    console.log('fetch request : ' + e.request.url)
-    e.respondWith(
-        caches.match(e.request).then(function(request) {
-            if (request) { // if cache is available, respond with cache
-                console.log('responding with cache : ' + e.request.url)
-                return request
-            } else { // if there are no cache, try fetching request
-                console.log('file is not cached, fetching : ' + e.request.url)
-                return fetch(e.request)
-            }
-
-            // You can omit if/else for console.log & put one line below like this too.
-            // return request || fetch(e.request)
-        })
-    )
-})
-
-// Cache resources
+/* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
     e.waitUntil(
-        caches.open(CACHE_NAME).then(function(cache) {
-            console.log('installing cache : ' + CACHE_NAME)
-            return cache.addAll(URLS)
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(filesToCache);
         })
-    )
-})
+    );
+    self.skipWaiting();
+});
 
-// Delete outdated caches
-self.addEventListener('activate', function(e) {
-    e.waitUntil(
-        caches.keys().then(function(keyList) {
-            // `keyList` contains all cache names under your username.github.io
-            // filter out ones that has this app prefix to create white list
-            var cacheWhitelist = keyList.filter(function(key) {
-                    return key.indexOf(APP_PREFIX)
-                })
-                // add current cache name to white list
-            cacheWhitelist.push(CACHE_NAME)
-
-            return Promise.all(keyList.map(function(key, i) {
-                if (cacheWhitelist.indexOf(key) === -1) {
-                    console.log('deleting cache : ' + keyList[i])
-                    return caches.delete(keyList[i])
-                }
-            }))
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
         })
-    )
-})
+    );
+});
