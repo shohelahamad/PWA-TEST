af73f1ed4109555b7feb9a593f988f061aff23bd
Md Shohel Ahamad <shohel.ahamad@alphajump.de>
2021-08-31 09:13:37 +0200

    added icons

--- js/main.js
+++ js/main.js
@@ -5,4 +5,21 @@ window.onload = () => {
         navigator.serviceWorker
             .register('./sw.js');
     }
+    var myDynamicManifest = {
+        "name": "meinOrt Brey",
+        "short_name": "brey",
+        "description": "meinOrt Brey",
+        "start_url": "https://shohelahamad.github.io/PWA-TEST/index.html",
+        "background_color": "#000000",
+        "theme_color": "#0f4a73",
+        "icons": [{
+            "src": "../images/Wappen_Brey.png",
+            "sizes": "256x256",
+            "type": "image/png"
+        }]
+    }
+    const stringManifest = JSON.stringify(myDynamicManifest);
+    const blob = new Blob([stringManifest], { type: 'application/json' });
+    const manifestURL = URL.createObjectURL(blob);
+    document.querySelector('#my-manifest-placeholder').setAttribute('href', manifestURL);
 }
