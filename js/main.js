69f4839eb1d7e8254587cea944357ed271cc294c
Md Shohel Ahamad <shohel.ahamad@alphajump.de>
2021-08-31 09:36:11 +0200

    adeed item

--- js/main.js
+++ js/main.js
@@ -5,5 +5,20 @@ window.onload = () => {
         navigator.serviceWorker
             .register('./sw.js');
     }
+
+    var link = document.querySelector("link[rel~='icon']");
+    if (!link) {
+        link = document.createElement('link');
+        link.rel = 'icon';
+        document.getElementsByTagName('head')[0].appendChild(link);
+    }
+    let main_city_id = true;
+    // let main_city_id = JSON.parse(window.localStorage.getItem("main_city"));
+    if (main_city_id) {
+        link.href = '../PWA-TEST/images/Wappen_Brey.png';
+    } else {
+        link.href = 'favicon.ico';
+    }
+
     document.querySelector('#my-manifest-placeholder').setAttribute('href', '../PWA-TEST/mainfest_Brey.json');
 }
