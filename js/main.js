27cedc9481bf0d86187bbf28d3667ed962ef70a6
Md Shohel Ahamad <shohel.ahamad@alphajump.de>
2021-08-31 09:18:43 +0200

    dynamic mf file

--- js/main.js
+++ js/main.js
@@ -5,21 +5,5 @@ window.onload = () => {
         navigator.serviceWorker
             .register('./sw.js');
     }
-    var myDynamicManifest = {
-        "name": "meinOrt Brey",
-        "short_name": "brey",
-        "description": "meinOrt Brey",
-        "start_url": "https://shohelahamad.github.io/PWA-TEST/index.html",
-        "background_color": "#000000",
-        "theme_color": "#0f4a73",
-        "icons": [{
-            "src": "https://shohelahamad.github.io/PWA-TEST/images/Wappen_Brey.png",
-            "sizes": "256x256",
-            "type": "image/png"
-        }]
-    }
-    const stringManifest = JSON.stringify(myDynamicManifest);
-    const blob = new Blob([stringManifest], { type: 'application/json' });
-    const manifestURL = URL.createObjectURL(blob);
-    document.querySelector('#my-manifest-placeholder').setAttribute('href', manifestURL);
+    document.querySelector('#my-manifest-placeholder').setAttribute('href', '../mainfest_Brey.json');
 }
